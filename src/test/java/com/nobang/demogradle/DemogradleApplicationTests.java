94ca59020aca3ec9196c273eac82cae7d31aed95
dogfootman <idogfootman@gmail.com>
2022-01-28 14:16:32 +0900

    Initial commit

---
+++ src/test/java/com/nobang/demogradle/DemogradleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nobang.demogradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemogradleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
