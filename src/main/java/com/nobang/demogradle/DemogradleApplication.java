43156f5cac600b07606fd6fbcbe76f3c8a6f50b9
dogfootman <idogfootman@gmail.com>
2022-01-28 14:20:16 +0900

    here

--- src/main/java/com/nobang/demogradle/DemogradleApplication.java
+++ src/main/java/com/nobang/demogradle/DemogradleApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemogradleApplication {
 
     public static void main(String[] args) {
+        System.out.println("here");
         SpringApplication.run(DemogradleApplication.class, args);
     }
 
